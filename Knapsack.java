be0abc6339c8b3713a3138c368612fb780e3b256
Prayas Mahakur <104716871+pritum-4@users.noreply.github.com>
2024-10-04 19:25:01 +0530

    Create Knapsack.java

---
+++ Knapsack.java
@@ -0,0 +1,31 @@
+package Dynamic_Programming;
+
+public class Knapsack {
+    public static int knapsack(int[] values, int[] weights, int capacity) {
+        int n = values.length;
+        int[][] dp = new int[n + 1][capacity + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= capacity; j++) {
+                if (weights[i - 1] <= j) {
+                    dp[i][j] = Math.max(values[i - 1] + dp[i - 1][j - weights[i - 1]], dp[i - 1][j]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[n][capacity];
+    }
+
+    public static void main(String[] args) {
+        int[] values = {60, 100, 120};
+        int[] weights = {10, 20, 30};
+        int capacity = 50;
+
+        int maxVal = knapsack(values, weights, capacity);
+
+        System.out.println("Max value: " + maxVal);
+    }
+}
+
